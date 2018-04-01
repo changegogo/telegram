6a4e9f8afa3a7eb9ce3c74aaa8841190ca86764f
dailliwang <758017188@qq.com>
2018-04-01 13:59:02 +0800

    db add user

--- routes/apply.js
+++ routes/apply.js
@@ -1,27 +1,27 @@
 /**
  * 提币申请
  */
-var express = require('express');
-var router = express.Router();
-var commonUtils = require('../utils/commonUtils');
-var Player = require('../schemaDao/Player');
-var Applycan = require('../schemaDao/Applycan');
-var threshold = 1880; // 提币阀值
+const express = require('express');
+const router = express.Router();
+const commonUtils = require('../utils/commonUtils');
+const Player = require('../schemaDao/Player');
+const Applycan = require('../schemaDao/Applycan');
+const threshold = 1880; // 提币阀值
 /**
  * 获取验证码
  */
 router.post('/smsapi', function(req, res, next){
     // 获取用户识别码
-    var identitycode = req.body.identitycode;
+    let identitycode = req.body.identitycode;
     // 验证用户识别码
     if(identitycode) {
         // 临时方案
         //identitycode = identitycode.replace(/ /g, '+');
-        var aes = commonUtils.aesidentitycode(identitycode);
+        let aes = commonUtils.aesidentitycode(identitycode);
         if(aes.length === 2) {
             // 用户识别码有效
             // 随机生成验证码
-            var code = commonUtils.generateCode();
+            let code = commonUtils.generateCode();
             // 写入session
             req.session.smscode = code;
             res.json({code: 200, msg: "短信验证码获取成功", smscode: req.session.smscode});
@@ -44,7 +44,7 @@ router.post('/', function(req, res, next){
         return;
     }
     // 获取用户识别码
-    var identitycode = req.body.identitycode;
+    let identitycode = req.body.identitycode;
     // 临时方案
     identitycode = identitycode.replace(/ /g, '+');
     if(!identitycode){
@@ -52,14 +52,14 @@ router.post('/', function(req, res, next){
         return;
     }
     // 验证验证码
-    var sessioncode = req.session.smscode;
+    let sessioncode = req.session.smscode;
     if(!commonUtils.verifySmscode(smscode, sessioncode)){
         res.json({code: 10017, msg: "验证码不正确"});
         return;
     }
     req.session.smscode = null;
     // 验证用户识别码
-    var phoneimtoken = commonUtils.aesidentitycode(identitycode);
+    let phoneimtoken = commonUtils.aesidentitycode(identitycode);
     if(phoneimtoken.length===0){
         res.json({code: 10018, msg: "用户识别码不正确"});
         return;
@@ -67,9 +67,9 @@ router.post('/', function(req, res, next){
     // 程序执行到这里，说明用户识别码和验证码都没有问题
     // 申请提币操作 >188,才允许提币
     //phoneimtoken[0]
-    var telphone = phoneimtoken[0];
-    var imtoken = phoneimtoken[1];
-    var query = {
+    let telphone = phoneimtoken[0];
+    let imtoken = phoneimtoken[1];
+    let query = {
         telphone: telphone,
         imtoken: imtoken
     };
@@ -77,14 +77,14 @@ router.post('/', function(req, res, next){
         if(!err){
             if(player){
                 // 获取累计can币数
-                var totalcancount = player.totalcancount;
+                let totalcancount = player.totalcancount;
                 // 获取已申请提can币数
-                var haspickupcount = player.haspickupcount;
+                let haspickupcount = player.haspickupcount;
                 //  当前未提币的数量
-                var offsetcount = totalcancount - haspickupcount;
+                let offsetcount = totalcancount - haspickupcount;
                 if(offsetcount >= threshold) {
                     // 修改Player库中的已提币数目
-                    var sum = haspickupcount + offsetcount;
+                    let sum = haspickupcount + offsetcount;
                     Player.updateOne(query, {
                         $set:{
                             haspickupcount: sum,
@@ -93,8 +93,8 @@ router.post('/', function(req, res, next){
                     }, function(err, c){
                             console.log(c);
                             // 向库中插入提币申请数据
-                            var ip = commonUtils.getIp(req);
-                            var applycan = {
+                            let ip = commonUtils.getIp(req);
+                            let applycan = {
                                 telphone: telphone,
                                 imtoken: imtoken,
                                 ip: ip,
