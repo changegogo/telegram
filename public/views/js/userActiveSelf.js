12378ec66499f77a856c451bbd7deb68ddc701e1
HengliYang <1029884642@qq.com>
2018-04-12 15:23:53 +0800

    bug fix

--- public/views/js/userActiveSelf.js
+++ public/views/js/userActiveSelf.js
@@ -1,15 +1,17 @@
 // getIdentifycode() ;
 $(function () {
-        var qrCodeimgUrl = window.location.protocol + '//' + window.location.host + "/telegram/index.html?identitycode=";
-        var qrcode = $('.qrCode-img-container').qrcode({
-            render: "canvas", //也可以替换为table
-            width: 100,
-            height: 100,
-            text: 'https://www.baidu.com'/*可以通过ajax请求动态设置*/
-        }).hide();
-        //将生成的二维码转换成图片格式
-        var canvas = qrcode.find('canvas').get(0);
-        $('#qrcodeImg').attr('src', canvas.toDataURL('image/jpg'));
+    $("#bigbig-img").css({height: document.body.clientHeight + 'px'})
+    // document.body.style.overflow='hidden';
+    var qrCodeimgUrl = window.location.protocol + '//' + window.location.host + "/telegram/index.html?identitycode=" +getIdentifycode();
+    var qrcode = $('.qrCode-img-container').qrcode({
+        render: "canvas", //也可以替换为table
+        width: 100,
+        height: 100,
+        text: qrCodeimgUrl/*可以通过ajax请求动态设置*/
+    }).hide();
+    //将生成的二维码转换成图片格式
+    var canvas = qrcode.find('canvas').get(0);
+    $('#qrcodeImg').attr('src', canvas.toDataURL('image/jpg'));
 
     html2canvas(document.querySelector("#userActiveSelf"),{
         allowTaint:true,
