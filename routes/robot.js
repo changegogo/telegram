e85d4c279c43ea79a42e433d67af308d10936c8c
dailliwang <758017188@qq.com>
2018-04-12 17:31:56 +0800

    robot

--- routes/robot.js
+++ routes/robot.js
@@ -21,7 +21,7 @@ router.post('/', function(req, res, next) {
 
     // 遍历每一个回复规则，查询是否有匹配的规则 还未测试
     function valuesArr(map){
-        let valuesArray = new Array();
+    let valuesArray = new Array();
         for (let kv of map) {
             if(!(typeof(kv[1])=="function")){     
                 valuesArray.push(kv[1]);     
