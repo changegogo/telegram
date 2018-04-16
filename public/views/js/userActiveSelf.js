282733d42e1bbbc1b5bdda05b6b6b5f3eea9bbfe
HengliYang <1029884642@qq.com>
2018-04-16 15:16:20 +0800

    bug fix

--- public/views/js/userActiveSelf.js
+++ public/views/js/userActiveSelf.js
@@ -1,13 +1,16 @@
 $(function () {
-    productImgMethod()
+    console.log(document.getElementsByTagName('head')[0].style.display = 'block')
+    // productImgMethod()
     function productImgMethod() {
         if (getQueryString('identitycode')) {
             var qrCodeimgUrl = window.location.protocol + '//' + window.location.host + "/telegram/index.html?identitycode=" +getQueryString('identitycode');
         } else {
             window.location.href = window.location.protocol + '//' + window.location.host + "/telegram/index.html"
         }
+
         var qrcode = $('.qrCode-img-container').qrcode({
-            render: "canvas", //也可以替换为table
+            // render: "canvas", //也可以替换为table
+            correctLevel:0,
             width: 100,
             height: 100,
             text: qrCodeimgUrl/*可以通过ajax请求动态设置*/
@@ -15,18 +18,37 @@ $(function () {
         //将生成的二维码转换成图片格式
         var canvas = qrcode.find('canvas').get(0);
         $('#qrcodeImg').attr('src', canvas.toDataURL('image/jpg'));
+        // htmlFomatCanvasMethod() ;
+        imgMethodAjax() ;
         $(document).click(function (event) {
             event.preventDefault();
         }) ;
-        htmlFomatCanvasMethod() ;
     }
    function htmlFomatCanvasMethod() {
-       html2canvas($("#userActiveSelf"),{
-       }).success(function (canvas) {
-           $("#userActiveSelf").hide() ;
-           var image = new Image();
-           image.src = canvas.toDataURL("image/png");
-           $("#bigbig-img").append(image);
+       html2canvas($("#userActiveSelf"), {
+           }).then(function (canvas) {
+               $("#userActiveSelf").hide();
+               var image = $("#tuiguangImg");
+               image.attr({src: canvas.toDataURL("image/jpg")});
+           });
+   }
+    imgMethodAjax()
+
+    function imgMethodAjax() {
+       $.ajax({
+           url: '../../genposter',
+           type: 'post',
+           data: {
+               identitycode: getQueryString('identitycode')
+           },
+           success: function(res){
+               console.log(JSON.stringify(res));
+               $('#tuiguangImg').attr('src', '../../poster/'+res.path);
+               // $('#tuiguangImg').attr('src', 'http://192.168.1.141:8890/poster/'+res.path);
+           },
+           error: function(err){
+               console.log(err);
+           }
        });
    }
 });
